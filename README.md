5238021e854045ab59d09a40d85da96db1175d24
Tianai Zhao <zhang201pig@126.com>
2015-11-23 20:51:16 -0800

    Start codes for d3.js demo

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # d3-demo
+This folder contains starter codes for the video I did about introduction to d3.js
---
+++ js/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+//Create a data
+ var data = [1,2,3,4,5]
+
+//Using d3.js to create some graphics
+//Select circle to bind data
+d3.select('#chart')
+	.append('circle')       //Append an element
+    //Assign attribute
+    .attr('r', 15)          
+    .attr('cx', 50)         
+    .attr('cy', 50)         
+    .attr('fill', 'red')    
+
+
+//Change the radius of each circle
+
+//Create another data
+//var data = [1,2,3]
+//Select circle and bind new data to it
+//Circles with new data in dark green color
+
+
+//Remove the red circles which is not related to the new data
+
+
+
+//A more complex data file
+// var dataset = [
+// 	{name: 'Mary', age: 40},
+// 	{name: 'John', age: 10},
+// 	{name: 'Peter', age: 30},
+//  {name: 'Katrina', age: 20}
+// ];
+
+//Create a div in your html and then append paragraphs
+//To show the names in the dataset
+
+
