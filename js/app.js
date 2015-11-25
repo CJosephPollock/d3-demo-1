74dbbf87755ec9b5150a11e83cc12a3629c21435
Joe Pollock <c.joseph.pollock@gmail.com>
2015-11-25 14:29:23 -0800

    TianAi Challenge Complete

--- js/app.js
+++ js/app.js
@@ -5,28 +5,33 @@
 
 //Using d3.js to create some graphics
 //Select circle to bind data
-d3.select('#chart')
-	.append('circle')       //Append an element
-    //Assign attribute
-    .attr('r', 15)          
-    .attr('cx', 50)         
-    .attr('cy', 50)         
-    .attr('fill', 'red')    
+var circleChart = d3.select('#chart').attr('height', 300)
+
 
 
 //Change the radius of each circle, so r increments by 5 at a time
+var circles = circleChart.selectAll('circle').data(data).enter()
+	.append('circle')       //Append an element
+    //Assign attribute
+    .attr('r', 15)          
+    .attr('cx', function(d) {return d * 30})         
+    .attr('cy', function(d) {return d * 30})         
+    .attr('fill', 'red');
 
 //Create another data
-//var data = [1,2,3]
+var lessData = [1,2,3]
 //Select circle and bind new data to it
+
+var circles = circleChart.selectAll('circle').data(lessData).attr('fill', 'purple')
 //Circles with new data in dark green color
 
 
 //Remove the red circles which is not related to the new data
+circles.exit().transition().attr("r",'0').duration(1000).remove();
 
 
 
-//A more complex data file
+// A more complex data file
 // var dataset = [
 // 	{name: 'Mary', age: 40},
 // 	{name: 'John', age: 10},
